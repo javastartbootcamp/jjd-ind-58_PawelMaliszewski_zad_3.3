a01991f54383016be0bfb929d0f0a52b65e5fd7d
Pawel Maliszewski <pablo999455@gmail.com>
2023-01-09 11:45:02 +0100

    solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -2,7 +2,23 @@ package pl.javastart.task;
 
 public class Main {
 
+    @SuppressWarnings("checkstyle:WhitespaceAfter")
     public static void main(String[] args) {
+        int evenOddNumber = 5;
+        double circleRadius = 4.5;
+        int toTheSecondPower = 8;
+
+        MathTasks mathTasks = new MathTasks();
+        boolean evenResult = mathTasks.isEven(evenOddNumber);
+        boolean oddResult = mathTasks.isOdd(evenOddNumber);
+        double circleField = mathTasks.circleField(circleRadius);
+        int power = mathTasks.power(toTheSecondPower);
+
+        System.out.println("Liczba: " + evenOddNumber + " jest parzysta: " + evenResult);
+        System.out.println("Liczba: " + evenOddNumber + " jest nieparzysta: " + oddResult);
+        System.out.printf("%s%.2f%s%.2fn", "Jeżeli promień koła wynośi: ", circleRadius, " to pole kola = ", circleField);
+        System.out.println();
+        System.out.println("Liczba " + toTheSecondPower + " do potęgi 2 = " + power);
 
     }
 }
